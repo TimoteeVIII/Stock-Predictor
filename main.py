e4d228a85041d0cde183fb35c739bdf47b49be8f
Kassim Chaudhry <kashi_chaudhry@hotmail.co.uk>
2022-08-20 00:29:28 +0100

    test

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
 from passlib.hash import bcrypt
-
+#hello
 app = Flask(__name__)
 
 # Change this to your secret key (can be anything, it's for extra protection)
